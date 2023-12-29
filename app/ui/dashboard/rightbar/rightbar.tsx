3e197c28671189cd71ca30b3a8ae07863dbf827e
muhammet-celik-90 <mhmmtclk90@gmail.com>
2023-12-29 09:29:57 +0100

    Announcement to be empty

--- app/ui/dashboard/rightbar/rightbar.tsx
+++ app/ui/dashboard/rightbar/rightbar.tsx
@@ -1,14 +1,13 @@
-import * as React from 'react';
-import { Stack } from '@mui/material';
-import Announcement from './announcement';
+import * as React from "react";
+import { Stack } from "@mui/material";
+import Announcement from "./announcement";
 
-export interface IRightbarProps {
-}
+export interface IRightbarProps {}
 
-export default function Rightbar (props: IRightbarProps) {
+export default function Rightbar(props: IRightbarProps) {
   return (
     <Stack spacing={2}>
-      <Announcement/>
+      {/* <Announcement /> */}
     </Stack>
   );
 }

4f0418bd4b4c34099c1fb0984ecd413a2d061234
muhammet-celik-90 <mhmmtclk90@gmail.com>
2023-12-29 09:08:03 +0100

    Kesme işareti hatası düzeltildi

--- app/ui/dashboard/rightbar/announcement.tsx
+++ app/ui/dashboard/rightbar/announcement.tsx
@@ -56,7 +56,7 @@ export default function Announcement(props: IAnnouncementProps) {
           sx={{ color: theme.palette.text.secondary, my: 1 }}
         >
           Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          industry. Lorem Ipsum has been the industrys standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book.
         </Typography>

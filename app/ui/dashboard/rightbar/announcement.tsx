4f533f7b685c72e9d12906c9406dc226945f4fb2
muhammet-celik-90 <mhmmtclk90@gmail.com>
2023-12-29 09:20:12 +0100

    Announcement text is editted

--- app/ui/dashboard/rightbar/announcement.tsx
+++ app/ui/dashboard/rightbar/announcement.tsx
@@ -55,10 +55,7 @@ export default function Announcement(props: IAnnouncementProps) {
           variant="subtitle2"
           sx={{ color: theme.palette.text.secondary, my: 1 }}
         >
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industrys standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book.
+          There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isnt anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
         </Typography>
         <Button
           variant="contained"
